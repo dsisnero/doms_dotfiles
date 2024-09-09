d8cd3dd36405c1eeb2b6293b8a5f2f7ccd09fc00
Dominic Sisneros <dsisnero@gmail.com>
2024-09-09 17:49:36 -0600

    cleanup rust cookbook

--- cookbooks/rust/default.rb
+++ cookbooks/rust/default.rb
@@ -3,20 +3,37 @@
 
 node.reverse_merge!({
   rust: {
-    version: 'stable',
+    version: "stable",
     user: node[:user]
   }
 })
 
 include_recipe "rust::user"
+user = node[:user]
+home = node[:home]
 
-unless ENV["PATH"].include?("#{node[:home]}/.cargo/bin:")
-  MItamae.logger.info("Prepending ~/.cargo/bin to PATH during this execution")
-  ENV["PATH"] = "#{node[:home]}/.cargo/bin:#{ENV["PATH"]}"
+cargo_home = node[:rust][:cargo_home]
+cargo_bin_dir = "#{cargo_home}/bin"
+rustup = "#{cargo_bin_dir}/rustup"
+cargo_cmd = "#{cargo_bin_dir}/cargo"
+
+cargo_env = <<~EOS
+  export PATH=#{cargo_bin_dir}:$PATH
+EOS
+
+unless ENV["PATH"].include?(cargo_bin_dir)
+  MItamae.logger.info("Prepending ~/.cargo/bin to PATH during this execution ")
+  ENV["PATH"] = "#{cargo_bin_dir}:#{ENV["PATH"]}"
 end
 
-rustup = "#{node[:home]}/.cargo/bin/rustup"
-cargo_cmd = "#{node[:home]}/.cargo/bin/cargo"
+# execute "install rust-src" do
+#   user user
+#   not_if do
+#     puts "ENV path for rustup: #{ENV["PATH"]}"
+#     `rustup component list | grep installed`.split("\n").any? { _1 =~ /rust-src/ }
+#   end
+#   command "#{cargo_env} ; rustup component add rust-src"
+# end
 
 # execute "sudo -E -u #{node[:user]} #{rustup} component add rust-src" do
 #   not_if "sudo -E -u #{node[:user]} #{rustup} component list | grep 'rust-src (installed)' >/dev/null"
@@ -25,11 +42,11 @@ cargo_cmd = "#{node[:home]}/.cargo/bin/cargo"
 # add RUSTC_WRAPPER to ENV
 unless ENV["RUSTC_WRAPPER"]
   MItamae.logger.info("adding RUSTC_WRAPPER to ENV during this execution")
-  ENV["RUSTC_WRAPPER"] = "#{node[:home]}/.cargo/bin/sccache"
+  ENV["RUSTC_WRAPPER"] = "#{cargo_bin_dir}/sccache"
 end
 
 cargo_init = <<~EOS
-  ENV['RUSTC_WRAPPER'] = "#{node[:home]}/.cargo/bin/sccache"
+  ENV['RUSTC_WRAPPER'] = "#{cargo_bin_dir}/sccache"
 EOS
 
 # define cargo_install command
@@ -52,30 +69,18 @@ define :cargo, version: nil, locked: true, path: nil, git: nil,
     end
   end
 
-# add RUSTC_WRAPPER to .bashrc
-execute "add RUSTC_WRAPPER to .bashrc" do
-  command "echo 'export RUSTC_WRAPPER=#{ENV["HOME"]}/.cargo/bin/sccache' >> #{ENV["HOME"]}/.bashrc"
-  command "source #{ENV["HOME"]}/.bashrc"
-  not_if "grep 'export RUSTC_WRAPPER' #{ENV["HOME"]}/.bashrc"
+file "#{home}/.bashrc" do
+  action :edit
+  not_if "grep 'source $HOME/.cargo/env' #{home}/.bashrc"
+  block do |content|
+    content << "source $HOME/.cargo/env"
+  end
 end
 
-# # install helix
-# git "helix" do
-#   repository "https://github.com/helix-editor/helix"
-#   destination "#{ENV["HOME"]}/src/helix"
-#   user node[:user]
-#   revision "HEAD"
-# end
-#
-# execute "helix compile" do
-#   cwd "#{ENV["HOME"]}/src/helix"
-#   user node[:user]
-#   command "#{cargo_cmd} install --path helix-term --locked"
-#   not_if "which hx"
-# end
-#
-# # add HELIX_RUNTIME to .bashrc
-# execute "add HELIX_RUNTIME to .bashrc" do
-#   command "echo 'export HELIX_RUNTIME=#{ENV["HOME"]}/src/helix/runtime' >> #{ENV["HOME"]}/.bashrc"
-#   not_if "grep 'export HELIX_RUNTIME' #{ENV["HOME"]}/.bashrc"
-# end
+file "#{home}/.bashrc" do
+  action :edit
+  not_if "grep 'export RUSTC_WRAPPER' #{home}/.bashrc"
+  block do |content|
+    content << %(export RUSTC_WRAPPER=#{cargo_bin_dir}/.sccache)
+  end
+end
