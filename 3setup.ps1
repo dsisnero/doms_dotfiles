828fd63e584f85f509a4a686b6c34a5bb6d7652d
Dominic Sisneros <dsisnero@gmail.com>
2024-09-11 15:03:01 -0600

    update setup script for to link config files

--- 3setup.ps1
+++ 3setup.ps1
@@ -1,5 +1,5 @@
 $ErrorActionPreference = 'Stop'
-
+$windows_home = 'C:\windows_home'
 mkdir -Force -p ${HOME}/repos/github.com/dsisnero/
 git clone https://github.com/dsisnero/doms_dotfiles.git ${HOME}/repos/github.com/dsisnero/doms_dotfiles
 
@@ -8,11 +8,12 @@ mkdir -Force -p "$HOME\AppData\Local\Microsoft\Windows Terminal"
 mkdir -Force -p "$HOME\Documents\PowerShell"
 mkdir -Force -p "$HOME\.config"
 
-New-Item -Value "$HOME\repos\github.com\dsisnero\dotfiles\config\WindowsTerminal\settings.json" -Path "$HOME\AppData\Local\Microsoft\Windows Terminal" -Name settings.json -ItemType SymbolicLink
-New-Item -Value "$HOME\repos\github.com\dsisnero\dotfiles\config\Microsoft.PowerShell_profile.ps1" -Path "$HOME\Documents\PowerShell" -Name Microsoft.PowerShell_profile.ps1 -ItemType SymbolicLink
-New-Item -Value "$HOME\repos\github.com\dsisnero\dotfiles\config\.gitconfig" -Path "$HOME\" -Name .gitconfig -ItemType SymbolicLink
-New-Item -Value "$HOME\repos\github.com\dsisnero\dotfiles\config\.config\wezterm" -Path "$HOME\.config\" -Name wezterm -ItemType SymbolicLink
-New-Item -Value "$HOME\repos\github.com\dsisnero\dotfiles\config\CorvusSKK" -Path "$HOME\AppData\Roaming\" -Name CorvusSKK -ItemType SymbolicLink
+New-Item -Value "$windows_home\repos\github.com\dsisnero\doms_dotfiles\config\WindowsTerminal\settings.json" -Path "$HOME\AppData\Local\Microsoft\Windows Terminal" -Name settings.json -ItemType SymbolicLink
+New-Item -Value "$windows_home\repos\github.com\dsisnero\doms_dotfiles\config\Microsoft.PowerShell_profile.ps1" -Path "$HOME\Documents\PowerShell" -Name Microsoft.PowerShell_profile.ps1 -ItemType SymbolicLink
+New-Item -Value "$windows_home\repos\github.com\dsisnero\doms_dotfiles\config\.config\git\config" -Path "$windows_home\.config\git" -Name config -ItemType SymbolicLink
+New-Item -Value "$windows_home\repos\github.com\dsisnero\doms_dotfiles\config\.config\wezterm" -Path "$windows_home\.config\" -Name wezterm -ItemType SymbolicLink
+New-Item -Value "$windows_home\repos\github.com\dsisnero\doms_dotfiles\config\.config\helix\config.toml" -Path "$windows_home\.config\helix" -Name config.toml -ItemType SymbolicLink
+New-Item -Value "$windows_home\repos\github.com\dsisnero\doms_dotfiles\config\.config\helix\languages.toml" -Path "$windows_home\.config\helix" -Name languages.toml -ItemType SymbolicLink
 
 # wingetデデキルヨ
 # Storeアプリインストールしてちょ
