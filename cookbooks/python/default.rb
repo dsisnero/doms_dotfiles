290d50c27110cff25422e1e2f982c4d464034c4b
Dominic Sisneros <dsisnero@gmail.com>
2025-03-24 14:32:53 -0600

    refactor: migrate python installation from asdf to mise

--- cookbooks/python/default.rb
+++ cookbooks/python/default.rb
@@ -1,8 +1,9 @@
 include_recipe "./dependency.rb"
 include_cookbook "uv"
-version = "latest"
-# version = node[:python][:version] unless node[:python].nil?
-version = "3.13.0"
+include_cookbook "mise"
+
+version = node[:python][:version]
+version ||= "latest"
 
 user = node[:user]
 home = node[:home]
@@ -13,14 +14,19 @@ remote_file "#{home}/.default-python-packages" do
   mode "644"
 end
 
-[
-  {cmd: "asdf plugin add python https://github.com/asdf-community/asdf-python.git",
-   not_if: "asdf plugin list | grep python"},
-  {cmd: "asdf install python #{version}", not_if: "asdf list python | grep #{version}"},
-  {cmd: "asdf global python #{version}", not_if: "which python"}
-].each do |op|
-  source_asdf_and_execute op[:cmd] do
-    user user
-    not_if_ op[:not_if] unless op[:not_if].nil?
-  end
+execute "install latest python" do
+  user user
+  command %(mise use -g python@#{version})
 end
+
+# [
+#   {cmd: "asdf plugin add python https://github.com/asdf-community/asdf-python.git",
+#    not_if: "asdf plugin list | grep python"},
+#   {cmd: "asdf install python #{version}", not_if: "asdf list python | grep #{version}"},
+#   {cmd: "asdf global python #{version}", not_if: "which python"}
+# ].each do |op|
+#   source_asdf_and_execute op[:cmd] do
+#     user user
+#     not_if_ op[:not_if] unless op[:not_if].nil?
+#   end
+# end
