b71d3761ceae1f76c0d063db49bbc12c0b4dfc6b
Dominic Sisneros <dsisnero@gmail.com>
2025-03-24 17:13:06 -0600

    refactor: remove redundant Specinfra configuration from recipe

--- lib/recipe.rb
+++ lib/recipe.rb
@@ -1,17 +1,3 @@
-# FIRST configure Specinfra before any platform detection
-require 'specinfra'
-Specinfra.configuration.os[:family] = 'ubuntu' if node[:platform] == 'pop'
-
-# THEN define the command module
-module Specinfra
-  module Command
-    module Pop
-      class Base < Specinfra::Command::Ubuntu::Base
-      end
-    end
-  end
-end
-
 include_recipe "recipe_helper"
 
 # FINALLY set platform override
