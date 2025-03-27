720a326042bf4c7b9f9f1b4c9ddd4da55de9975e
Dominic Sisneros <dsisnero@gmail.com>
2025-03-27 12:40:07 -0600

    same recipe for simple_completion-language_server for all

--- cookbooks/simple_language_server/default.rb
+++ cookbooks/simple_language_server/default.rb
@@ -1,37 +1,7 @@
 # include_recipe 'dependency.rb'
-
-case node[:platform]
-when "debian", "ubuntu", "mint", "pop"
+include_cookbook "ghq"
   get_repo "estin/simple-completion-language-server" do
     build <<-EOCMD
     mise exec -- ./cargo install --path .
     EOCMD
   end
-when "fedora", "redhat", "amazon"
-  get_repo "etsin/simple-completion-language-server" do
-    build <<-EOCMD
-    mise exec -- ./cargo install --path .
-    EOCMD
-  end
-when "osx", "darwin"
-  package "migemo"
-when "arch"
-  package "migemo"
-when "opensuse"
-  # nothing to do
-when "windows"
-  get_repo "etsin/simple-completion-language-server" do
-    build <<-EOCMD
-    mise exec -- ./cargo install --path .
-    EOCMD
-  end
-end
-get_repo "github.com/estin/simple-completion-language-server",
-  repository: "estin/simple-completion-language-server",
-  build: <<-EOCMD
-    cd #{home}/repos/github.com/estin/simple-completion-language-server
-    cargo install --path .
-  EOCMD,
-  version_cmd: "simple-completion-language-server --version",
-  version_str: "0.1.0"
-)
