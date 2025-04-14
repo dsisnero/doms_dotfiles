840e49d50b35840465049053dd1108652458443c
Dominic Sisneros <dsisnero@gmail.com>
2025-04-14 10:58:35 -0600

    add uv aider, llm, and docling to uv cookbook

--- cookbooks/uv/default.rb
+++ cookbooks/uv/default.rb
@@ -1,4 +1,24 @@
-execute "curl -LsSf https://astral.sh/uv/install.sh | sh" do
-  user node[:user]
-  not_if "which uv"
+include_cookbook "mise"
+
+mise "uv"
+
+execute "install aider" do
+  command <<~EOCMD
+    mise exec -- uv tool install --force --python python3.12 aider-chat
+  EOCMD
+  not_if %(which aider )
+end
+
+execute "install docling" do
+  command <<~EOCMD
+    mise exec -- uv tool install docling
+  EOCMD
+  not_if %(which docling)
+end
+
+execute "install llm" do
+  command <<~EOCMD
+    mise exec -- uv tool install llm
+  EOCMD
+  not_if %(which llm)
 end
