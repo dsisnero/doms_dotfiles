c98363f87019f93f1b42e2a517b229a8682f55d6
Dominic Sisneros <dsisnero@gmail.com>
2025-04-14 10:58:15 -0600

    add some repos to myrepos

--- cookbooks/myrepos/default.rb
+++ cookbooks/myrepos/default.rb
@@ -1,12 +1,22 @@
 # download myrepos
-user = node[:user]
+node[:user]
 
 myrepos = [
-  # "dsisnero/doms_dotfiles",
-  # "dsisnero/nexcom-srf",
-  # "dsisnero/autocad"
+  "doms_dotfiles",
+  "nexcom-srf",
+  "faa-cost_estimate",
+  "autocad",
+  "astronvim_config",
+  "faa_project",
+  "mitamae",
+  "xdg",
+  "keyring",
+  "gmaps",
+  "cltk",
+  "perdiem",
+  "shell2batch.cr"
 ]
-myrepos.each { |name| get_repo name }
+myrepos.each { |name| get_repo "dsisnero/#{name}" }
 
 directory "~/.local/share/fonts" do
   owner node[:user]
@@ -21,9 +31,7 @@ end
 # install_font "#{node[:home]}/repos/gitlab.com/dsisnero/RictyDiminished-for-Powerline/Ricty_Diminished_Bold_for_Powerline.ttf"
 # install_font "#{node[:home]}/repos/gitlab.com/dsisnero/RictyDiminished-for-Powerline/Ricty_Diminished_Bold_Oblique_for_Powerline.ttf"
 # install_font "#{node[:home]}/repos/gitlab.com/dsisnero/RictyDiminished-for-Powerline/Ricty_Diminished_Oblique_for_Powerline.ttf"
-
-blog_repo_path = "~/repos/github.com/dsisnero/blog.dsisnero"
-obsidian_vault_path = "~/repos/github.com/dsisnero/private-memo/obsidian/work"
+"~/repos/github.com/dsisnero/private-memo/obsidian/work"
 # execute "ln -s #{blog_repo_path} #{obsidian_vault_path}/blog" do
 #   not_if "test -e #{obsidian_vault_path}/blog"
 #   user user
