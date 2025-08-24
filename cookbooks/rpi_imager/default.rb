4ed9d5ab8197d89454a8e0513704878f9fd33383
Dominic Sisneros <dsisnero@gmail.com>
2025-08-23 23:06:04 -0600

    rpi_imager scoped to platform

--- cookbooks/rpi_imager/default.rb
+++ cookbooks/rpi_imager/default.rb
@@ -1,7 +1,12 @@
-package "snapd"
-
 # Install rpi-imager using system commands since it requires classic confinement
-execute "install rpi-imager" do
-  command "snap install rpi-imager --classic"
-  not_if "snap list | grep -q rpi-imager"
+case node[:platform]
+
+when "debian", "ubuntu", "mint", "pop"
+  package "snapd"
+
+  execute "install rpi-imager" do
+    command "snap install rpi-imager --classic"
+    not_if "snap list | grep -q rpi-imager"
+  end
+when "osx"
 end
