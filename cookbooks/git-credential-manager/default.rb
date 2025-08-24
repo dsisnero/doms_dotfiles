5b5a673eb8700fcb24d6206f1893101a3ef917e5
Dominic Sisneros <dsisnero@gmail.com>
2025-08-23 23:07:15 -0600

    add package install for darwin (git-credential-manager

--- cookbooks/git-credential-manager/default.rb
+++ cookbooks/git-credential-manager/default.rb
@@ -4,7 +4,7 @@ case node[:platform]
 when "arch"
   raise NotImplementedError
 when "osx", "darwin"
-  raise NotImplementedError
+  package 'git-credential-manager'
 when "fedora", "redhat", "amazon"
   raise NotImplementedError
 
