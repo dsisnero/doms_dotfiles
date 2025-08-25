77510cc2561b87da2b35a281b9483a0f8028d1eb
Dominic Sisneros <dsisnero@gmail.com>
2025-08-25 14:54:49 -0600

    add package keychain for darwin, zlogin uses keychain

--- cookbooks/keychain/default.rb
+++ cookbooks/keychain/default.rb
@@ -3,6 +3,7 @@ include_recipe "dependency.rb"
 case node[:platform]
 when "arch"
 when "osx", "darwin"
+  package "keychain"
 when "fedora", "redhat", "amazon"
 when "debian", "ubuntu", "mint"
   package "keychain"
