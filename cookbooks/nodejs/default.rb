d8696e6649a42e69f28883773deb3b9c32e3d9ad
Dominic Sisneros <dsisnero@gmail.com>
2025-09-15 21:34:39 -0600

    add .default-npm-packages for node

--- cookbooks/nodejs/default.rb
+++ cookbooks/nodejs/default.rb
@@ -1,12 +1,29 @@
 include_recipe "./dependency.rb"
 include_cookbook "./mise"
 
+user = node["user"]
+home = node["home"]
+
 # node.reverse_merge!({
 #   nodejs: {
 #     version: '10.13.0',
 #   }
 # })
+
+remote_file "#{home}/.default-npm-packages" do
+  source "files/.default-npm-packages"
+  owner user
+  mode "644"
+end
+
+node.reverse_merge!(
+  node: {
+    version: "latest"
+  }
+)
+version = node[:node][:version] || "latest"
+
 execute "install node" do
   user node[:user]
-  command "mise use -g node"
+  command "mise use -g node@#{version}"
 end
