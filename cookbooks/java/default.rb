425681d8cf520bfb58565379868e5fe4fdea9324
Dominic Sisneros <dsisnero@gmail.com>
2025-09-17 11:26:28 -0600

    change package java to cookbook

---
+++ cookbooks/java/default.rb
@@ -0,0 +1,14 @@
+case node[:platform]
+when "debian", "ubuntu", "mint", "pop"
+when "darwin"
+  package "java"
+
+  src = "/opt/homebrew/opt/openjdk/libexec/openjdk.jdk"
+  dst = "/Library/Java/JavaVirtualMachines/openjdk.jdk"
+
+  link dst do
+    to src
+    not_if "test -d #{dst}"
+    user "root"
+  end
+end
