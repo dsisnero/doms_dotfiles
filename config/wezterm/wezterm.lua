6215198dd260fdb815b181064222bf8b53e9eab6
Dominic Sisneros <dsisnero@gmail.com>
2025-09-17 11:23:44 -0600

    move config/.config/wezterm to config/wezterm

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -77,26 +77,27 @@ local config = {
 	-- font = wezterm.font("HackGenNerd Console"), -- 自分の好きなフォントいれる
 	-- font = wezterm.font("Fira Code"), -- 自分の好きなフォントいれる
 	font = wezterm.font_with_fallback({
-		"Fira Code",
+		"JetBrainsMono Nerd Font",
 		"HackGenNerd Console",
 		"Symbols Nerd Font Mono",
 	}),
-	font_size = 12.0,
+	font_size = 13.0,
 	-- color_scheme = "iceberg-dark", -- 自分の好きなテーマ探す https://wezfurlong.org/wezterm/colorschemes/index.html
 	color_scheme = "nord", -- 自分の好きなテーマ探す https://wezfurlong.org/wezterm/colorschemes/index.html
 	-- window_background_opacity = 0.8,
-	-- window_frame = {
-	--   inactive_titlebar_bg = '#353535',
-	--   active_titlebar_bg = '#2b2042',
-	--   inactive_titlebar_fg = '#cccccc',
-	--   active_titlebar_fg = '#ffffff',
-	--   inactive_titlebar_border_bottom = '#2b2042',
-	--   active_titlebar_border_bottom = '#2b2042',
-	--   button_fg = '#cccccc',
-	--   button_bg = '#2b2042',
-	--   button_hover_fg = '#ffffff',
-	--   button_hover_bg = '#3b3052',
-	-- },
+	window_frame = {
+		font = wezterm.font({ family = "Noto Sans", weight = "Regular" }),
+		--   inactive_titlebar_bg = '#353535',
+		--   active_titlebar_bg = '#2b2042',
+		--   inactive_titlebar_fg = '#cccccc',
+		--   active_titlebar_fg = '#ffffff',
+		--   inactive_titlebar_border_bottom = '#2b2042',
+		--   active_titlebar_border_bottom = '#2b2042',
+		--   button_fg = '#cccccc',
+		--   button_bg = '#2b2042',
+		--   button_hover_fg = '#ffffff',
+		--   button_hover_bg = '#3b3052',
+	},
 
 	text_background_opacity = 1.0,
 	hide_tab_bar_if_only_one_tab = false,
@@ -104,7 +105,7 @@ local config = {
 	------------------------------------------
 	-- Key Binding
 	-- timeout_milliseconds defaults to 1000 and can be omitted
-	leader = { key = "s", mods = "CTRL", timeout_milliseconds = 1001 },
+	leader = { key = "a", mods = "CTRL", timeout_milliseconds = 1001 },
 	disable_default_key_bindings = true,
 	keys = keybinds.create_keybinds(),
 	key_tables = keybinds.key_tables,
