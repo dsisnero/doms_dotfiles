d4a485491c0e0d59249f300bf387d2338cf595c9
Dominic Sisneros <dsisnero@gmail.com>
2025-09-24 16:56:05 -0600

    feat: add dotfile definitions

---
+++ definitions/dotfile.rb
@@ -0,0 +1,14 @@
+# dotfileリポジトリ内へのシンボリックリンク設定
+define :dotfile, source: nil, user: nil do
+  dst = File.join(node[:config_home], params[:name])
+  src = params[:source].nil? ? File.join(node[:doms_dotfiles], "config", params[:name]) : params[:source]
+  user = params[:user].nil? ? node[:user] : params[:user]
+  # puts "dst: #{dst}"
+  # puts "src: #{src}"
+
+  link dst do
+    to src
+    user user
+    not_if { run_specinfra("check_is_symlink(#{dst})") }
+  end
+end
