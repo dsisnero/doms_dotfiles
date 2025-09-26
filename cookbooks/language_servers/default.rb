4b341728636dbdd73a6fd7797b514efe57963d3c
Dominic Sisneros <dsisnero@gmail.com>
2025-09-25 22:20:40 -0600

    add taplo and tombi language servers

--- cookbooks/language_servers/default.rb
+++ cookbooks/language_servers/default.rb
@@ -11,7 +11,7 @@ when "darwin"
   package "docker-ls"
   package "ansible-language-server"
 
-  npm_installed = %w[vscode-langservers-extracted typescript typescript-language-server]
+  npm_installed = %w[vscode-langservers-extracted typescript typescript-language-server tombi]
 
   npm_installed.each do |lsp|
     execute("installing #{lsp}") do
@@ -24,6 +24,7 @@ when "darwin"
 
   mise "marksman"
   mise "dprint"
+  mise "taplo"
   # Copy dprint config file
   remote_file "#{config_home}/dprint/config.json" do
     source "files/dprint_config.json"
