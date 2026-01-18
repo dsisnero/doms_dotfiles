cfbf29fb48dcf9caea77858be75865c89e5c1a43
Dominic Sisneros <dsisnero@gmail.com>
2026-01-18 13:07:59 -0700

    merged from osx

--- lib/recipe.rb
+++ lib/recipe.rb
@@ -2,4 +2,9 @@
 MItamae.logger.info "Detected platform: #{node[:platform].inspect}"
 # Ensure OS is consistent with platform changes
 include_recipe "recipe_helper"
+if node[:platform] == "pop"
+  node.reverse_merge!(
+    platform: "ubuntu"
+  )
+end
 include_role node[:platform]
