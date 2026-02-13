a71d2715a5dd68508527faab2707ce42154ce170
Dominic Sisneros <dsisnero@gmail.com>
2026-02-13 02:28:15 -0700

    minimal package for small systems

--- roles/minimal/default.rb
+++ roles/minimal/default.rb
@@ -1,1 +1,15 @@
 # Minimal role - no additional cookbooks beyond platform defaults
+include_cookbook "dotfiles"
+include_cookbook "mise"
+
+mise "rg"
+mise "rclone"
+mise "bottom"
+mise "broot" do
+  backend "cargo"
+end
+
+include_cookbook "ruby" # git hookスクリプトで必要なので先にインストールする'
+include_cookbook "python"
+include_cookbook "yarn"
+include_cookbook "node"
