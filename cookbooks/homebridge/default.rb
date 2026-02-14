621b28b5007d414f63c52568857583c93c668e0a
Dominic Sisneros <dsisnero@gmail.com>
2026-02-13 21:27:36 -0700

    add homebridge cookbook

---
+++ cookbooks/homebridge/default.rb
@@ -0,0 +1,12 @@
+case node[:platform]
+  when darwin
+    execute("install homebridge.key") do
+      command <<~CONTENT
+        curl -sSfL https://repo.homebridge.io/KEY.gpg | sudo gpg --dearmor | sudo tee /usr/share/keyrings/homebridge.gpg  > /dev/null
+        echo "deb [signed-by=/usr/share/keyrings/homebridge.gpg] https://repo.homebridge.io stable main" | sudo tee /etc/apt/sources.list.d/homebridge.list > /dev/null
+      CONTENT
+      user root
+    end
+end
+
+
