d1bd98286350d63e4e39d8e1d5e1278f4b12c05c
Dominic Sisneros <dsisnero@gmail.com>
2026-05-19 12:51:21 -0600

    gh fixes for running with mise

--- cookbooks/github-cli/default.rb
+++ cookbooks/github-cli/default.rb
@@ -21,7 +21,7 @@ if rasppi?
         && apt install gh -y
     EOH
     user "root"
-    not_if { run_command("gh version", error: false).exit_status == 0 }
+    not_if "which gh"
   end
 end
 
@@ -30,30 +30,30 @@ unless rasppi?
   execute "install github-cli" do
     user node[:user]
     command "mise use -g gh@#{node[:github_cli][:version]}"
-    not_if { run_command("gh version", error: false).exit_status == 0 }
+    not_if "mise exec -- which gh"
   end
 end
 
 # Authenticate with GitHub CLI
 execute "gh_auth_login" do
   user node[:user]
-  command "gh auth login --git-protocol ssh --hostname github.com"
-  not_if { run_command("gh auth status").stdout =~ /Logged in to github.com/ }
+  command "mise exec -- gh auth login --git-protocol ssh --hostname github.com"
+  not_if { run_command("mise exec -- gh auth status").stdout =~ /Logged in to github.com/ }
 end
 
 execute "gh_request_ssh_authoritation" do
   user node[:user]
-  command "gh auth refresh -h github.com -s admin:public_key"
+  command "mise exec -- gh auth refresh -h github.com -s admin:public_key"
   only_if {
-    result = run_command("gh ssh-key list", error: false)
+    result = run_command("mise exec -- gh ssh-key list", error: false)
     result.exit_status != 0 && result.stderr =~ /This API operation needs the "admin:public_key" scope/
   }
 end
 execute "gh_request_ssh_public_key" do
   user node[:user]
-  command "gh auth refresh -h github.com -s admin:ssh_signing_key"
+  command "mise exec -- gh auth refresh -h github.com -s admin:ssh_signing_key"
   only_if {
-    result = run_command("gh ssh-key list", error: false)
+    result = run_command("mise exec -- gh ssh-key list", error: false)
     result.exit_status != 0 && result.stderr =~ /To request it, run:  gh auth refresh -h github.com -s admin:ssh_signing_key/
   }
 end
