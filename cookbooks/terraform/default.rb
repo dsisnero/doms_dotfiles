96e123d33d676b357def5b2133c11e8abf623bf4
Dominic Sisneros <dsisnero@gmail.com>
2026-07-09 17:56:43 -0600

    feat(terraform): implement install for all platforms
    Replace raise NotImplementedError stubs with working install commands:
    - Arch: pacman -S terraform
    - macOS: brew tap hashicorp/tap + brew install hashicorp/tap/terraform
    - Fedora: dnf from HashiCorp RPM repo
    - RedHat: dnf from HashiCorp RPM repo
    - Amazon Linux: yum from HashiCorp RPM repo
    - Debian/Ubuntu/Mint: apt from HashiCorp APT repo (previously unguarded)
    - openSUSE: zypper from HashiCorp RPM repo
    - Add not_if guards for idempotency
    - Fallback: warn instead of raise on unsupported platforms

--- cookbooks/terraform/default.rb
+++ cookbooks/terraform/default.rb
@@ -3,11 +3,47 @@ include_recipe "dependency.rb"
 
 case node[:platform]
 when "arch"
-  raise NotImplementedError
+  execute "install terraform" do
+    command "sudo pacman -S --noconfirm terraform"
+    not_if "which terraform"
+  end
 when "osx", "darwin"
-  raise NotImplementedError
-when "fedora", "redhat", "amazon"
-  raise NotImplementedError
+  execute "tap hashicorp/tap" do
+    command "brew tap hashicorp/tap"
+    not_if "brew tap | grep -q '^hashicorp/tap$'"
+  end
+
+  execute "install terraform" do
+    command "brew install hashicorp/tap/terraform"
+    not_if "which terraform"
+  end
+when "fedora"
+  execute "install terraform" do
+    command <<~EOCMD
+      sudo dnf install -y dnf-plugins-core
+      sudo dnf config-manager --add-repo https://rpm.releases.hashicorp.com/fedora/hashicorp.repo
+      sudo dnf install -y terraform
+    EOCMD
+    not_if "which terraform"
+  end
+when "redhat"
+  execute "install terraform" do
+    command <<~EOCMD
+      sudo dnf install -y dnf-plugins-core
+      sudo dnf config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
+      sudo dnf install -y terraform
+    EOCMD
+    not_if "which terraform"
+  end
+when "amazon"
+  execute "install terraform" do
+    command <<~EOCMD
+      sudo yum install -y yum-utils
+      sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
+      sudo yum install -y terraform
+    EOCMD
+    not_if "which terraform"
+  end
 when "debian", "ubuntu", "mint"
   execute "install terraform" do
     command <<~EOCMD
@@ -15,9 +51,17 @@ when "debian", "ubuntu", "mint"
       echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
       sudo apt update -y && sudo apt install -y terraform
     EOCMD
+    not_if "which terraform"
   end
 when "opensuse"
-  raise NotImplementedError
+  execute "install terraform" do
+    command <<~EOCMD
+      sudo zypper install -y zypper
+      sudo zypper addrepo https://rpm.releases.hashicorp.com/openSUSE/hashicorp.repo
+      sudo zypper install -y terraform
+    EOCMD
+    not_if "which terraform"
+  end
 else
-  raise NotImplementedError
+  MItamae.logger.warn "terraform install not implemented for platform #{node[:platform]}"
 end
