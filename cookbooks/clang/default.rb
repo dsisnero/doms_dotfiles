4f48835824d68ca6e3a9d726cac12dbad51c0575
Dominic Sisneros <dsisnero@gmail.com>
2026-07-09 17:56:14 -0600

    feat(clang): implement install for all Linux platforms and macOS
    Replace raise NotImplementedError stubs with working install commands:
    - Arch: pacman -S clang
    - macOS: xcode-select --install (Xcode CLI Tools)
    - Fedora/RedHat: dnf install -y clang
    - Amazon Linux: yum install -y clang
    - Debian/Ubuntu/Mint: package resource (already present)
    - openSUSE: zypper install -y clang
    - Fallback: warn instead of raise on unsupported platforms

--- cookbooks/clang/default.rb
+++ cookbooks/clang/default.rb
@@ -2,15 +2,32 @@ include_recipe "dependency.rb"
 
 case node[:platform]
 when "arch"
-  raise NotImplementedError
+  execute "install clang" do
+    command "sudo pacman -S --noconfirm clang"
+    not_if "which clang"
+  end
 when "osx", "darwin"
-  raise NotImplementedError
-when "fedora", "redhat", "amazon"
-  raise NotImplementedError
+  execute "install clang (Xcode Command Line Tools)" do
+    command "xcode-select --install"
+    not_if "xcode-select -p"
+  end
+when "fedora", "redhat"
+  execute "install clang" do
+    command "sudo dnf install -y clang"
+    not_if "which clang"
+  end
+when "amazon"
+  execute "install clang" do
+    command "sudo yum install -y clang"
+    not_if "which clang"
+  end
 when "debian", "ubuntu", "mint"
   package "clang"
 when "opensuse"
-  raise NotImplementedError
+  execute "install clang" do
+    command "sudo zypper install -y clang"
+    not_if "which clang"
+  end
 else
-  raise NotImplementedError
+  MItamae.logger.warn "clang install not implemented for platform #{node[:platform]}"
 end
