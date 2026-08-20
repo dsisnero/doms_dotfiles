ef8418196fb1a3286218abbc32776340e39f2e92
Dominic Sisneros <dsisnero@gmail.com>
2026-08-20 11:42:03 -0600

    remove llvm-project from favorite repos update lazygit config

--- cookbooks/favorite_repos/default.rb
+++ cookbooks/favorite_repos/default.rb
@@ -6,7 +6,7 @@ repos = %w[
   fujiwara/isucon11-f
   itamae-kitchen/mitamae
   AstroNvim/astrocommunity
-  llvm/llvm-project
+  # llvm/llvm-project
   crystal-ameba/ameba
 ]
 repos.each { |name| get_repo name.chomp }
